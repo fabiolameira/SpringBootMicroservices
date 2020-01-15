849bb636be51cce6ed1a2297e58806755bdadb52
Fábio Lameira <famiranda@minsait.com>
2020-01-15 18:02:29 +0100

    Using WebClientBuilder to make the calls

--- movie-catalog-service/src/main/java/com/microservices/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/microservices/moviecatalogservice/resources/MovieCatalogResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.microservices.moviecatalogservice.models.CatalogItem;
 import com.microservices.moviecatalogservice.models.Movie;
@@ -21,15 +22,29 @@ public class MovieCatalogResource {
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	@Autowired
+	private WebClient.Builder webClientBuilder;
+	
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-		
+				
 		List<Rating> ratings = Arrays.asList(
 				new Rating("1001", 4),
 				new Rating("1002", 3),
 				new Rating("1003", 5));
 		
 		 return ratings.stream().map(rating -> {
+			 
+			 /* Utilizando o WebClientBuilder para fazer chamadas.
+			 Movie movie = webClientBuilder.build()
+			 	.get()
+			 	.uri("http://localhost:8082/movies/" + rating.getMovieId())
+			 	.retrieve()
+			 	.bodyToMono(Movie.class)
+			 	.block();
+		 	*/
+			 
+			 // Utilizando o RestTemplate para fazer chamadas.
 			 Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
 			 return new CatalogItem(movie.getName(), "Just an fast cars movie.", rating.getRating());
 		 })
